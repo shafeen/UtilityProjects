8371086598fc71c9f2c2efba0a444b50000a0843
Shafeen M <sodl02@gmail.com>
2016-02-23 23:44:41 -0500

    removed unused old function

--- LIVEdotREDIS/Frontend/ldi-front-util.js
+++ LIVEdotREDIS/Frontend/ldi-front-util.js
@@ -1,42 +1,10 @@
 var mainBody = $("#myBody").get(0);
 
-// alternative to createLDIView using handlebars
-// TODO: replace createLDIView with this
+// LIVEdotIO view
 function createLDIViewHandlebars(idPostfix) {
     var ldiViewTemplate = Handlebars.compile($("#ldiView-template").html());
     return $(ldiViewTemplate({idPostfix : idPostfix}))[0];
 }
-
-// LIVEdotIO view
-// TODO: remove this when no longer used
-function createLDIView(idPostfix) {
-    var ldiView = $("<div></div>");
-
-    var idPrefix = "";
-    $(ldiView).attr("id", idPrefix + idPostfix);
-
-    // the element's IDs must match the corresponding model's attribute names
-    var divHeading = $("<h4></h4>").attr("id", "divHead_" + idPostfix);
-    var divPara1 = $("<p></p>").attr("id",  "divPara1_" + idPostfix);
-    var divPara2 = $("<p></p>").attr("id", "divPara2_" + idPostfix);
-
-    divHeading.append("Heading "+idPostfix);
-    divPara1.append("Info 1");
-    divPara2.append("Info 2");
-
-    ldiView.append(divHeading)
-           .append($("<hr/>"))
-           .append(divPara1)
-           .append(divPara2);
-
-    ldiView.addClass("ldiView");
-    divHeading.addClass("ldiViewHead");
-    divPara1.addClass("ldiViewPara");
-    divPara2.addClass("ldiViewPara");
-
-    return ldiView.get(0);
-}
-
 
 function addNewLDIView(viewName) {
     var ldiView = createLDIViewHandlebars(viewName);
