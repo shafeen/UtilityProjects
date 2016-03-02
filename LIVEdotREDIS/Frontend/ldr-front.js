51455685ea3e0b33a941fe2ef90102bc52434477
Shafeen M <sodl02@gmail.com>
2016-03-02 00:29:49 -0500

    ldr-front.js code has now been namespaced it will no longer be polluting the global namespace

--- LIVEdotREDIS/Frontend/ldr-front.js
+++ LIVEdotREDIS/Frontend/ldr-front.js
@@ -1,75 +1,78 @@
-// TODO: namespace all of these functions
+// namespace containing ldr-front functions
+window.ldr = window.ldr || {};
 
-var redisKeyInput = $('#redisKeyInput');
-var redisKeyLabels = $('#redisKeyLabels');
+(function(ldr) {
+    var redisKeyInput = $('#redisKeyInput');
+    var redisKeyLabels = $('#redisKeyLabels');
 
-function getIdFromValue(value) {
-    return "key_" + value;
-}
+    ldr.getIdFromValue = function(value) {
+        return "key_" + value;
+    };
 
-// TODO: break out the utility functions to their own files
-function addRedisKeyLabel(redisKey) {
-    var redisKeyLabelTemplate = Handlebars.compile($('#redisKeyLabel-template').html());
-    var redisKeyLabel = $(redisKeyLabelTemplate({redisKey: redisKey}));
-    redisKeyLabel.hide();
-    redisKeyLabels.append(redisKeyLabel);
-    redisKeyLabel.fadeIn();
-}
+    // TODO: break out the utility functions to their own files
+    ldr.addRedisKeyLabel = function (redisKey) {
+        var redisKeyLabelTemplate = Handlebars.compile($('#redisKeyLabel-template').html());
+        var redisKeyLabel = $(redisKeyLabelTemplate({redisKey: redisKey}));
+        redisKeyLabel.hide();
+        redisKeyLabels.append(redisKeyLabel);
+        redisKeyLabel.fadeIn();
+    };
 
-function removeRedisKeyLabel(redisKey) {
-    var redisKeyLabel = $('#'+getIdFromValue(redisKey));
-    if(redisKeyLabel.length) {
-        // remove label gap and label after fading out
-        redisKeyLabel.fadeOut("slow", function() {
-            $(this).next().remove();
-            $(this).remove();
-        });
-    }
-    return (redisKeyLabel.length != 0);
-}
+    ldr.removeRedisKeyLabel = function(redisKey) {
+        var redisKeyLabel = $('#'+ldr.getIdFromValue(redisKey));
+        if(redisKeyLabel.length) {
+            // remove label gap and label after fading out
+            redisKeyLabel.fadeOut("slow", function() {
+                $(this).next().remove();
+                $(this).remove();
+            });
+        }
+        return (redisKeyLabel.length != 0);
+    };
 
-function getKeyTypeSelected() {
-    var keyTypeSelected = "KEY";
-    var keyTypeSelector = $("#keyTypeSelector");
-    if(keyTypeSelector) {
-        keyTypeSelected = keyTypeSelector.children()[keyTypeSelector.selectedIndex].text;
-    }
-    return keyTypeSelected;
-}
+    ldr.getKeyTypeSelected = function() {
+        var keyTypeSelected = "KEY";
+        var keyTypeSelector = $("#keyTypeSelector");
+        if(keyTypeSelector) {
+            keyTypeSelected = keyTypeSelector.children()[keyTypeSelector.selectedIndex].text;
+        }
+        return keyTypeSelected;
+    };
 
-function trackKey() {
-    if(!redisKeyInput.val()) {
-        alert("Type in a key first!");
-    } else if($('#'+getIdFromValue(redisKeyInput.val())).length) {
-        alert("That key is already being tracked!");
-        redisKeyInput.val("");
-    } else {
-        // TODO: don't allow whitespaces in keys
+    ldr.trackKey = function() {
+        if(!redisKeyInput.val()) {
+            alert("Type in a key first!");
+        } else if($('#'+ldr.getIdFromValue(redisKeyInput.val())).length) {
+            alert("That key is already being tracked!");
+            redisKeyInput.val("");
+        } else {
+            // TODO: don't allow whitespaces in keys
+            var redisKey = redisKeyInput.val();
+            redisKeyInput.val("");
+
+            ldr.addRedisKeyLabel(redisKey);
+
+            var keyTypeSelected = ldr.getKeyTypeSelected();
+            // Send the key to the backend to track it
+            var socket = io();
+            if(keyTypeSelected == "KEY") {
+                socket.emit('trackKey', redisKey);
+            } else if(keyTypeSelected == "HKEY") {
+                socket.emit('trackHKey', redisKey);
+            }
+        }
+    };
+
+    ldr.removeKey = function() {
         var redisKey = redisKeyInput.val();
         redisKeyInput.val("");
 
-        addRedisKeyLabel(redisKey);
-
-        var keyTypeSelected = getKeyTypeSelected();
-        // Send the key to the backend to track it
-        var socket = io();
-        if(keyTypeSelected == "KEY") {
-            socket.emit('trackKey', redisKey);
-        } else if(keyTypeSelected == "HKEY") {
-            socket.emit('trackHKey', redisKey);
+        if(ldr.removeRedisKeyLabel(redisKey) == true) {
+            // Send the key to the backend to stop tracking it
+            var socket = io();
+            socket.emit('removeKey', redisKey);
+        } else {
+            alert("No such key being tracked!")
         }
-    }
-}
-
-function removeKey() {
-    var redisKey = redisKeyInput.val();
-    redisKeyInput.val("");
-
-    if(removeRedisKeyLabel(redisKey) == true) {
-        // Send the key to the backend to stop tracking it
-        var socket = io();
-        socket.emit('removeKey', redisKey);
-    } else {
-        alert("No such key being tracked!")
-    }
-}
+    };
+})(window.ldr);
