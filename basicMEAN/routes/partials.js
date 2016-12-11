27c4df4bb761c86ce8115fe82b17677ea5673d61
Shafeen M <sodl02@gmail.com>
2016-12-11 02:20:51 -0500

    basicMEAN: partial view loading route is now more generic

--- basicMEAN/routes/partials.js
+++ basicMEAN/routes/partials.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var uuid = require('uuid/v4');
 
-// navbar
-router.get('/navbar/:viewname', function(req, res) {
-    res.render('partials/navbar/'+req.params.viewname);
+const VIEW_PARTIALS_FOLDER = 'partials';
+
+// loading module partial views
+router.get('/:modulename/:viewname', function(req, res) {
+    res.render(VIEW_PARTIALS_FOLDER+'/'+req.params.modulename+'/'+req.params.viewname);
 });
 
 module.exports = router;
