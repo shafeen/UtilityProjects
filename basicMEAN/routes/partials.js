87e2c4206c1bb4f7d695e9201580ca4592c86e19
Shafeen M <sodl02@gmail.com>
2016-12-12 23:06:23 -0500

    Conditional login/logout in navbar

--- basicMEAN/routes/partials.js
+++ basicMEAN/routes/partials.js
@@ -5,9 +5,13 @@ var uuid = require('uuid/v4');
 const VIEW_PARTIALS_PATH = 'partials/';
 const NG_CLIENT_RELATIVE_PATH = '../ng-client/';
 
-// loading module partial views
+// loading module partial views -- anything more specific should come before
 router.get('/:modulename/:partialname', function(req, res) {
-    res.render(NG_CLIENT_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname);
+    if (req.isAuthenticated()) {
+        res.render(NG_CLIENT_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname, {user: req.user});
+    } else {
+        res.render(NG_CLIENT_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname);
+    }
 });
 
 module.exports = router;
