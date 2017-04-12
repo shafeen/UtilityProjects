f4f3d2e2ad74b5357204f3cd38ec0c278f1019e8
shafeen <sodl02@gmail.com>
2017-04-12 15:52:15 -0400

    from now on, I will be using $ctrl to refer to view controllers as a convention

--- basicMEAN/ng-client/config.js
+++ basicMEAN/ng-client/config.js
@@ -3,18 +3,27 @@ angular.module('basicMEAN', ["ngRoute"])
     $routeProvider
         .when("/", {
             templateUrl: "partials/view1/view1",
-            controller: "View1Ctrl as view1"
+            controller: "View1Ctrl as $ctrl"
         })
         .when("/view1", {
             templateUrl: "partials/view1/view1",
-            controller: "View1Ctrl as view1"
+            controller: "View1Ctrl as $ctrl"
         })
         .when("/view2", {
             templateUrl: "partials/view2/view2",
-            controller: "View2Ctrl as view2"
+            controller: "View2Ctrl as $ctrl"
         })
         .when("/view-protected", {
             templateUrl: "partials/secure/view-protected/view-protected",
-            controller: "ViewProtectedCtrl as viewProtected"
+            controller: "ViewProtectedCtrl as $ctrl"
         });
 });
+
+/*
+To add a new view, follow these steps:
+  - create a folder for the view in ng-client OR ng-client-protected
+  - create a template pug file for the view
+  - create a controller for the view
+  - preload the controller's js file in layout.pug
+  - add a route for the view in the config above 
+*/
