0d26be2494203ba0d9281edb1150d8859263b0bc
Shafeen M <sodl02@gmail.com>
2017-08-20 17:27:30 -0400

    updated basicMEAN: partials loaded will now have access to variables in settings.json

--- basicMEAN/routes/partials.js
+++ basicMEAN/routes/partials.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var uuid = require('uuid/v4');
+const express = require('express');
+const router = express.Router();
+const settings = require('../config/settings/settings.json');
 
 const NG_CLIENT_RELATIVE_PATH = '../ng-client/';
 const NG_CLIENT_SECURE_RELATIVE_PATH = '../ng-client-secure/';
 
 // loading module partial views -- anything more specific should come before
 router.get('/:modulename/:partialname', function(req, res) {
-    res.render(NG_CLIENT_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname, {user: req.user});
+    res.render(NG_CLIENT_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname, {user: req.user, settings: settings});
 });
 
 // loading secure module partial views -- anything more specific should come before
 router.get('/secure/:modulename/:partialname', isLoggedIn, function(req, res) {
-    res.render(NG_CLIENT_SECURE_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname, {user: req.user});
+    res.render(NG_CLIENT_SECURE_RELATIVE_PATH + req.params.modulename + '/' + req.params.partialname, {user: req.user, settings: settings});
 });
 
 // route middleware to make sure a user is logged in
